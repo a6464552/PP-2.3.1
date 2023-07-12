b2202f4a71895579f105da770ff71393544d72f7
a6464552 <129376622+a6464552@users.noreply.github.com>
2023-07-12 11:52:47 +0300

    Add files via upload

---
+++ PP-2_3_1/src/main/java/web/dao/UserDao.java
@@ -0,0 +1,13 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+    void delete(long id);
+    User change(User user);
+    List<User> listUsers();
+    User findUserById(long id);
+}
